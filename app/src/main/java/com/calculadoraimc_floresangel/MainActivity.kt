dc9b1d8db5b00092b5f784abe9d964650870b86a
Bludegan <angel.flores204305@potros.itson.edu.mx>
2023-02-10 20:17:59 -0700

    Integracion del proyecto con el repositorio

---
+++ app/src/main/java/com/calculadoraimc_floresangel/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.calculadoraimc_floresangel
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var resultado: Float = 0f
+    var altura: Float = 0f
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val peso: EditText = findViewById(R.id.peso) as EditText
+        val estatura: EditText = findViewById(R.id.estatura) as EditText
+        val calcular: Button = findViewById(R.id.calcular) as Button
+        val imc: TextView = findViewById(R.id.imc) as TextView
+        val rango: TextView = findViewById(R.id.rango) as TextView
+
+        calcular.setOnClickListener {
+            altura = (estatura.text.toString().toFloat() * estatura.text.toString().toFloat())
+            resultado = (peso.text.toString().toFloat() / altura)
+            imc.setText(resultado.toString())
+
+            when(resultado){
+                in 0.0..18.4 -> {
+                    rango.setText("Bajo peso")
+                    rango.setBackgroundResource(R.color.greenish)
+                }
+                in 18.5..24.9 -> {
+                    rango.setText("Normal")
+                    rango.setBackgroundResource(R.color.green)
+                }
+                in 25.0..29.9->{
+                    rango.setText("Sobrepeso")
+                    rango.setBackgroundResource(R.color.yellow)
+                }
+                in 30.0..34.9->{
+                    rango.setText("Obesidad grado 1")
+                    rango.setBackgroundResource(R.color.orange)
+                }
+                in 35.0..39.9->{
+                    rango.setText("Obesidad grado 2")
+                    rango.setBackgroundResource(R.color.red)
+                }
+                in 40.0..100.0->{
+                    rango.setText("Obesidad grado 3")
+                    rango.setBackgroundResource(R.color.brown)
+                }
+                else -> {
+                    rango.setBackgroundResource(R.color.white)
+                }
+            }
+
+        }
+
+
+    }
+}
